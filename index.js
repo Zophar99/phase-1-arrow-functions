417b73b12f4b9e356a492434eced565fe6e29054
zophar <zophar.nyakundi@student.moringaschool.com>
2025-06-15 18:05:34 +0300

    phase-1-arrow-function

---
+++ index.js
@@ -0,0 +1,12 @@
+// Function expression for divide
+const divide = function(a, b) {
+  return a / b;
+};
+console.log("divide(2000, 100):", divide(2000, 100));
+
+// // Arrow function for square
+const square = (x) => x * x;
+console.log("square(5):", square(5));
+// Arrow function for add
+const add = (a, b) => a + b;
+console.log("add(3, 4):", add(3, 4)); 
